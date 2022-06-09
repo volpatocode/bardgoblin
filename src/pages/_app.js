c07eb5305c94e28f7fa5742c3a9de6ee063d1bad
João Volpato <volpatocode@gmail.com>
2022-06-08 21:01:49 -0300

    feat: boilerplate

---
+++ src/pages/_app.js
@@ -0,0 +1,18 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+type propsType = {};
+
+export default function Home(): propsType {
+  return <>Teste</>;
+}
