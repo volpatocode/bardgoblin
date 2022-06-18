7fe5166e31923077b07451a43f2c51a548c905dc
João Volpato <volpatocode@gmail.com>
2022-06-18 17:42:18 -0300

    changes: forgot password page template

--- src/pages/forgotPassword/forgotPassword.tsx
+++ src/pages/forgotPassword/forgotPassword.tsx
@@ -1,23 +1,10 @@
 import React, { useContext } from "react";
-import Navbar from "../../components/Navbar";
-import Footer from "../../components/Footer";
 import {
   ForgotPasswordWrapper,
-  ForgotPasswordContent,
   ForgotPasswordBox,
-  ImageBox,
-  Warning,
+  ForgotPasswordImageBox,
 } from "./styles";
 
-import {
-  BoxButtons,
-  BoxLogo,
-  FinishButton,
-  InputInfo,
-  InputTitle,
-} from "../../components/UserModal/styles";
-
-import Logo from "../../components/Logo";
 import { UserContext } from "../../contexts/UserContext";
 import { useForm } from "react-hook-form";
 
@@ -27,29 +14,8 @@ export default function index() {
 
   return (
     <ForgotPasswordWrapper>
-      <Navbar page="forgotpassword" />
-      <ForgotPasswordContent>
-        <ForgotPasswordBox>
-          <BoxLogo>
-            <Logo variant="vertical" />
-          </BoxLogo>
-          <form onSubmit={handleSubmit(forgotPassword)}>
-            <InputTitle>Recover your password</InputTitle>
-            <InputInfo
-              {...register("email")}
-              id="register-email"
-              type="email"
-            />
-            <BoxButtons>
-              <Warning>Check your spam box</Warning>
-              <FinishButton type="submit">Send email</FinishButton>
-            </BoxButtons>
-          </form>
-        </ForgotPasswordBox>
-
-        <ImageBox src="/campfire.png" />
-      </ForgotPasswordContent>
-      <Footer />
+      <ForgotPasswordBox>a</ForgotPasswordBox>
+      <ForgotPasswordImageBox/>
     </ForgotPasswordWrapper>
   );
 }
--- src/pages/forgotPassword/styles.ts
+++ src/pages/forgotPassword/styles.ts
@@ -4,39 +4,26 @@ import { Box } from "@mui/material";
 export const ForgotPasswordWrapper = styled(Box)`
   min-height: 100vh;
   min-width: 100vw;
-  background: url("/background.png") no-repeat center center / cover;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-`;
-export const ForgotPasswordContent = styled(Box)`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
-  width: 70%;
-  height: 100%;
-  gap: 5rem;
 `;
 
 export const ForgotPasswordBox = styled(Box)`
-  width: 100%;
-  height: 100%;
-  border-radius: 10px;
-  background-color: #151515;
-  display: flex;
-  flex-direction: column;
-  padding: 2rem 1rem;
-  text-align: left;
+  width:40vw;
+  height: 100vh;
+  background: #141414;
 `;
 
-export const ImageBox = styled.img`
-  width: 50%;
-  height: 50%;
-  object-fit: cover;
-  object-position: center;
+export const ForgotPasswordImageBox = styled(Box)`
+  width:60vw;
+  height: 100vh;
+  background: url("/backgroundcampfiremiddle.png") no-repeat center center / cover;
 `;
+
+
+
 
 export const Warning = styled.p`
   font-size: 0.7rem;
