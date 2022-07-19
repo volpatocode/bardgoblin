6ad2a6801f908c6b3b2a148a357e6f7f1cb88854
João Volpato <volpatocode@gmail.com>
2022-07-19 19:56:50 -0300

    changes: footer ui

--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -11,15 +11,15 @@ align-items: center;
 text-align: center;
 gap: 0.5rem;
 width: 100%;
-padding: 0.7rem 2.5rem 0.7rem 2.5rem;
+padding: 0.3rem 2.5rem 0.3rem 2.5rem;
 @media (max-width: 960px) {
-  padding: 0.7rem 1.5rem 0.7rem 1.5rem;
+  padding: 0.3rem 1.5rem 0.3rem 1.5rem;
 }
 @media (max-width: 600px) {
   justify-content: center;
 }
 
 @media (max-width: 400px) {
-  padding: 0.7rem 0.5rem 0.7rem 0.5rem;
+  padding: 0.3rem 0.5rem 0.3rem 0.5rem;
 }
 `;
