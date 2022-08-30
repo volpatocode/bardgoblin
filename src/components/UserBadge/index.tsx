8981e53aa8f0df13ebda33f113d653cc6318eee1
João Volpato <volpatocode@gmail.com>
2022-08-29 21:30:39 -0300

    feat: dynamic userBadge

--- src/components/UserBadge/index.tsx
+++ src/components/UserBadge/index.tsx
@@ -11,7 +11,7 @@ export default function index({photoURL, displayName}: badgeType) {
   return (
     <UserBadge>
       <UserImage src={photoURL || "https://www.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png"} />
-      <Username>{displayName}</Username>
+      <Username>{displayName || "User"}</Username>
     </UserBadge>
   );
 }
