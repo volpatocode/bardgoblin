0f9558236b40b1e0edd29d582da8827fba2acbe5
João Volpato <volpatocode@gmail.com>
2022-09-19 21:33:37 -0300

    feat: utils functions added to context

--- src/contexts/UtilsContext.tsx
+++ src/contexts/UtilsContext.tsx
@@ -1,15 +1,41 @@
+import { useRouter } from "next/router";
 import { createContext, ReactNode } from "react";
+import { useMediaQuery } from "@mui/material";
 
 type UtilsContextProps = {
   children: ReactNode;
 };
 
-type UtilsContextType = {};
+type UtilsContextType = {
+  screenSm: boolean;
+  screenMd: boolean;
+  forceHome: () => void;
+  refreshPage: () => void;
+};
 
 export const UtilsContext = createContext<UtilsContextType>(
   {} as UtilsContextType
 );
 
 export const UtilsContextProvider = ({ children }: UtilsContextProps) => {
-  return <UtilsContext.Provider value={{}}>{children}</UtilsContext.Provider>;
+  const router = useRouter();
+
+  function forceHome() {
+    router.push("/");
+  }
+
+  function refreshPage() {
+    router.reload();
+  }
+
+  const screenSm = useMediaQuery("(max-width:600px)");
+  const screenMd = useMediaQuery("(max-width:1000px)");
+
+  return (
+    <UtilsContext.Provider
+      value={{ screenMd, screenSm, refreshPage, forceHome }}
+    >
+      {children}
+    </UtilsContext.Provider>
+  );
 };
