0df442721da3c022e3dedf6e3a6806c69470eb3d
João Volpato <volpatocode@gmail.com>
2022-10-08 14:29:40 -0300

    feat: firebase environment

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "bardgoblin-1305b.firebaseapp.com",
-  projectId: "bardgoblin-1305b",
-  storageBucket: "bardgoblin-1305b.appspot.com",
-  messagingSenderId: "1057873027637",
-  appId: "1:1057873027637:web:7401ec548b31d0dab1477c",
-  measurementId: "G-WQFHZ2MX3L",
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
