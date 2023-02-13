915e0dd832f5f9f782d45b2e0ad4e0cf1dd7d975
João Volpato <volpatocode@gmail.com>
2023-02-12 21:46:33 -0300

    feat: user fixtures and add to tests

--- cypress/e2e/signUp.cy.js
+++ cypress/e2e/signUp.cy.js
@@ -3,55 +3,59 @@
 describe("sign up paths", () => {
   beforeEach(() => {
     cy.visit("/");
+    cy.fixture("user").then(function (data) {
+      this.data = data;
+    });
   });
 
-  it("write wrong email", () => {
+  it("write wrong email", function () {
     cy.getByData("navbar-avatar").click();
     cy.getByData("menu-list-item2").contains("Register").click();
-    cy.get("#register-email").type("fakegmail.com");
-    cy.get("#register-password").type("123456");
-    cy.get("#register-confirm-password").type("123456");
+    cy.get("#register-email").type(this.data.wrongEmail);
+    cy.get("#register-password").type(this.data.password);
+    cy.get("#register-confirm-password").type(this.data.password);
     cy.getByData("register-finish").click();
     cy.getByData("input-error").should("exist").should("have.length", 1);
   });
 
-  it("write wrong password", () => {
+  it("write wrong password", function () {
     cy.getByData("navbar-avatar").click();
     cy.getByData("menu-list-item2").contains("Register").click();
-    cy.get("#register-email").type("fake@gmail.com");
-    cy.get("#register-password").type("1234567");
-    cy.get("#register-confirm-password").type("123456");
+    cy.get("#register-email").type(this.data.email);
+    cy.get("#register-password").type(this.data.wrongPassword);
+    cy.get("#register-confirm-password").type(this.data.wrongPassword);
     cy.getByData("register-finish").click();
     cy.getByData("input-error").should("exist").should("have.length", 1);
   });
 
-  it("write wrong confirm password", () => {
+  it("write wrong confirm password", function () {
     cy.getByData("navbar-avatar").click();
     cy.getByData("menu-list-item2").contains("Register").click();
-    cy.get("#register-email").type("fake@gmail.com");
-    cy.get("#register-password").type("123456");
-    cy.get("#register-confirm-password").type("1234567");
+    cy.get("#register-email").type(this.data.email);
+    cy.get("#register-password").type(this.data.password);
+    cy.get("#register-confirm-password").type(this.data.wrongPassword);
     cy.getByData("register-finish").click();
     cy.getByData("input-error").should("exist").should("have.length", 1);
   });
 
-  it("write wrong all inputs", () => {
+  it("write wrong all inputs", function () {
     cy.getByData("navbar-avatar").click();
     cy.getByData("menu-list-item2").contains("Register").click();
-    cy.get("#register-email").type("fakegmail.com");
-    cy.get("#register-password").type("12345");
-    cy.get("#register-confirm-password").type("123");
+    cy.get("#register-email").type(this.data.wrongEmail);
+    cy.get("#register-password").type(this.data.wrongPassword);
+    cy.get("#register-confirm-password").type(this.data.password);
     cy.getByData("register-finish").click();
-    cy.getByData("input-error").should("exist").should("have.length", 3);
+    cy.getByData("input-error").should("exist").should("have.length", 2);
   });
 
-  it("sign up in bardgoblin", () => {
+  it("sign up in bardgoblin", function () {
     cy.getByData("navbar-avatar").click();
     cy.getByData("menu-list-item2").contains("Register").click();
-    cy.get("#register-email").type("fake@gmail.com");
-    cy.get("#register-password").type("123456");
-    cy.get("#register-confirm-password").type("123456");
+    cy.get("#register-email").type(this.data.email);
+    cy.get("#register-password").type(this.data.password);
+    cy.get("#register-confirm-password").type(this.data.password);
     cy.getByData("register-finish").click();
     cy.getByData("input-error").should("exist").should("have.length", 1);
+    // add delete account post account creation
   });
 });
